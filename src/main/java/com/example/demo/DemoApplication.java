81c5126e66dd78b9574a942d850248c250aee9ae
1m4nim <fancustard9@gmail.com>
2026-01-09 12:25:17 +0900

    CSSをあてておみくじっぽくした

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -39,23 +39,44 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 
 @SpringBootApplication
-@RestController
+@Controller
 public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@GetMapping("/")
-	public String hello() {
-		return "Hello, World";
-	}
-
-	@GetMapping("/luck")
-	public String fortune() {
+	@GetMapping("/profile")
+	public String profile(@RequestParam(value = "name", defaultValue = "1m4nim") String name, Model model) {
 		String[] results = { "大吉", "中吉", "小吉", "凶" };
-		int r = new java.util.Random().nextInt(results.length);
-		return "今日の運勢は・・・" + results[r] + "です";
+		String luck = results[new java.util.Random().nextInt(results.length)];
+
+		model.addAttribute("name", name);
+		model.addAttribute("luck", luck);
+
+		return "hello";
 	}
 }
+
+// @GetMapping("/")
+// public String hello() {
+// return "Hello, World";
+// }
+
+// @GetMapping("/luck")
+// public String fortune() {
+// String[] results = { "大吉", "中吉", "小吉", "凶" };
+// int r = new java.util.Random().nextInt(results.length);
+// return "今日の運勢は・・・" + results[r] + "です";
+// }
+
+// @GetMapping("/greet")
+// public String greet(@RequestParam(value = "name", defaultValue = "ゲスト")
+// String name) {
+// return "Hello," + name + "さん！SpringBootの世界へようこそ";
+// }
+
+// }
