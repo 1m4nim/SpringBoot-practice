5485585697b8abf85acc2dd5b5d818e58f38f13b
1m4nim <fancustard9@gmail.com>
2026-01-09 11:32:21 +0900

    おみくじ

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,12 +12,33 @@
 
 // }
 
+// package com.example.demo;
+
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RestController;
+
+// @SpringBootApplication
+// @RestController
+// public class DemoApplication {
+// 	public static void main(String[] args) {
+// 		SpringApplication.run(DemoApplication.class, args);
+// 	}
+
+// 	@GetMapping("/")
+// 	public String hello() {
+// 		return "Hello, World";
+// 	}
+// }
+
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
 @RestController
@@ -29,5 +50,12 @@ public class DemoApplication {
 	@GetMapping("/")
 	public String hello() {
 		return "Hello, World";
+	}
+
+	@GetMapping("/luck")
+	public String fortune() {
+		String[] results = { "大吉", "中吉", "小吉", "凶" };
+		int r = new java.util.Random().nextInt(results.length);
+		return "今日の運勢は・・・" + results[r] + "です";
 	}
 }
